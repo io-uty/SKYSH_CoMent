19d53f559d041de631e65bfcc4a20ee14f064032
ohwj00 <ohwonjin20@gmail.com>
2026-06-28 12:58:14 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
+
 import React from "react";
+import ReactDOM from "react-dom/client";
+import Onboarding from "./Onboarding.tsx"; // 1. 온보딩 파일 임포트
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Onboarding /> {/* 2. 온보딩 컴포넌트 실행 */}
+  </React.StrictMode>,
+);
+/*import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
@@ -8,3 +19,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>,
 );
+*/
